47fe92b4ceceeb08a18944ab6bc5325362749d92
aaa-WilliamIbarraI <william.ibarrai@perficient.com>
2025-06-09 21:53:39 -0500

    Separate the routes in a diff folder ans using Router from express

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false })); // This help us to decode the de body, works as a middleware
 
-app.use("/", (req, res, next) => {
-  console.log("This always runs!");
-  next();
-});
-
-app.use("/add-product", (req, res, next) => {
-  console.log("In another middleware!");
-  res.send(
-    "<form action='/product' method='POST'><input  type='text' name='title'/> <button type='submit'> Submit</button> </form>"
-  );
-});
-
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  console.log("In another middleware!");
-  res.send("<h1>Hello from Express!</h1>");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
