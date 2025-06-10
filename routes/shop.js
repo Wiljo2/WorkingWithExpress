4af61a0d25cca2dcc779eae09fa8581fe2e9cdd1
aaa-WilliamIbarraI <william.ibarrai@perficient.com>
2025-06-09 22:22:26 -0500

    Serving html

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
   //router.get changing this to get, we avoid check the order in the app because is a exact match
-  console.log("In another middleware!");
-  res.send("<h1>Hello from Express!</h1>");
+  res.sendFile(path.join(__dirname, "..", "views", "shop.html"));
 });
 
 module.exports = router;
