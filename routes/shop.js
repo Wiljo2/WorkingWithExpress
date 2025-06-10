f957aec119cc1cf22ade7a1ad6f2f76382e4eb6b
aaa-WilliamIbarraI <william.ibarrai@perficient.com>
2025-06-09 22:35:57 -0500

    Using the a cleaner approach for the paths

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const path = require("path");
+const rootDir = require("../utils/path");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
   //router.get changing this to get, we avoid check the order in the app because is a exact match
-  res.sendFile(path.join(__dirname, "..", "views", "shop.html"));
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
