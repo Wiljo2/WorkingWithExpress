045a8e35c32d773c286f050e8a02e96f5d7063db
aaa-WilliamIbarraI <william.ibarrai@perficient.com>
2025-06-09 21:56:01 -0500

    Adding the method for the router to enable the exact match

--- routes/shop.js
+++ routes/shop.js
@@ -2,7 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 
-router.use("/", (req, res, next) => {
+router.get("/", (req, res, next) => {
+  //router.get changing this to get, we avoid check the order in the app because is a exact match
   console.log("In another middleware!");
   res.send("<h1>Hello from Express!</h1>");
 });
